095b0179997bb747860b6f34aba4a743cdab3ae1
Adam Petrone <adampetrone83@gmail.com>
2014-07-16 22:04:14 -0400

    stripped out useless class; added strip1 and strip2

--- httplightcontrol_ws2812.ino
+++ httplightcontrol_ws2812.ino
@@ -1,46 +1,55 @@
 #include <XBee.h>
-#include <SPI.h>
 
+// Designed for use with an Arduino Pro Mini 5v
 
-#include "wireless_neopixel.h"
+#include "wirelessclient.h"
 #include <Adafruit_NeoPixel.h>
 
 XBee xbee = XBee();
 
-const uint8_t NUM_LEDS = 32;
-const uint8_t DATA_PIN = 10;
-const uint8_t CLOCK_PIN = 11;
+const uint8_t NUM_LEDS = 30;
+const uint8_t DATA_PIN1 = 10;
+const uint8_t DATA_PIN2 = 7;
 
-Adafruit_NeoPixel strip( NUM_LEDS, DATA_PIN );
-WirelessNeoPixel relay( &strip );
+Adafruit_NeoPixel strip1( NUM_LEDS, DATA_PIN1 );
+Adafruit_NeoPixel strip2( NUM_LEDS, DATA_PIN2 );
+WirelessClient relay;
+
+void set_color( Adafruit_NeoPixel& strip, uint8_t r, uint8_t g, uint8_t b )
+{
+  for( int i = 0; i < strip.numPixels(); ++i )
+  {
+    strip.setPixelColor( i, strip.Color(r, g, b) );
+  }
+} // set_color
 
 void on_control_command(WirelessClient * lc, uint8_t * data, uint8_t data_length)
 {
-    uint8_t color = data[0];
-    relay.transition_color( color, COLOR_TRANSITION_TIME );
+    uint8_t r = data[0];
+    uint8_t g = data[1];
+    uint8_t b = data[2];
+    set_color(strip1, r, g, b);
+    set_color(strip2, r, g, b);    
+    strip1.show();
 }
 
 
 void setup()
 { 
   Serial.begin(9600);
-#if USE_SOFTWARE_SERIAL > 0
-  ss.begin(9600);
-  xbee.setSerial( ss );
-#else
   xbee.setSerial( Serial );
-#endif
   
   // setup light client commands
   relay.map_command( CMD_CONTROL_LIGHT, on_control_command );
   
   // initially off
-  relay.set_color( 0 );
+  set_color( strip1, 0, 0, 0 );
+  set_color( strip2, 0, 0, 0 );  
+  strip1.show();
+  strip2.show();
 }
 
 void loop()
 {
     relay.read_packet( xbee );
-    relay.show();
 }
-
